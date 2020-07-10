453a10a9e24748e343921405738d35a446ea4476
Shivam Kumar <shivamkr.lko@gmail.com>
2020-07-11 00:46:17 +0530

    task 1 and task 2

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -3,7 +3,7 @@ import org.junit.Assert;
 
 public class StringCalculatorTest {
     @Test
-    public void name() {
+    public void task1AndTask2() {
         StringCalculator stringCalculator = new StringCalculator();
         int charDigitSum = stringCalculator.Add("");
         Assert.assertEquals(0, charDigitSum);
@@ -12,5 +12,6 @@ public class StringCalculatorTest {
         Assert.assertEquals(1, stringCalculator.Add("1,"));
         Assert.assertEquals(2, stringCalculator.Add(",2"));
         Assert.assertEquals(3, stringCalculator.Add(",1,2,"));
+        Assert.assertEquals(3, stringCalculator.Add(",,,1,,,,2,,,,"));
     }
 }

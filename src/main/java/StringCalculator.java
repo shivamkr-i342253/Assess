5ef0375f74bfb67d7e98d5db107673c991faa2e4
Shivam Kumar <shivamkr.lko@gmail.com>
2020-07-11 00:40:03 +0530

    task 1 and 2

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,8 +1,20 @@
 public class StringCalculator {
     public int Add(String s) {
         if (s.length() != 0) {
-
+            // check for delimeter
+            if (s.contains(",")) {
+                // split the given string by delimeter
+                String[] nos = s.split(",");
+                int res = 0;
+                for (String z : nos) {
+                    if (z.length() != 0) res += Integer.parseInt(z);
+                    res += 0;
+                }
+                return res;
+            }
+            return Integer.parseInt(s);
         }
+        // null string
         return 0;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -7,5 +7,10 @@ public class StringCalculatorTest {
         StringCalculator stringCalculator = new StringCalculator();
         int charDigitSum = stringCalculator.Add("");
         Assert.assertEquals(0, charDigitSum);
+        Assert.assertEquals(1, stringCalculator.Add("1"));
+        Assert.assertEquals(0, stringCalculator.Add(","));
+        Assert.assertEquals(1, stringCalculator.Add("1,"));
+        Assert.assertEquals(2, stringCalculator.Add(",2"));
+        Assert.assertEquals(3, stringCalculator.Add(",1,2,"));
     }
 }
